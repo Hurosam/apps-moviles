5ede8889f6a7a2a64ed569179c5c83900c46fc39
Hurosam <2021110360@udh.edu.pe>
2025-07-08 00:11:48 -0500

    Lista predefinida de categorias

--- app/src/main/java/com/example/ta4/AppDatabase.java
+++ app/src/main/java/com/example/ta4/AppDatabase.java
@@ -1,11 +1,15 @@
 package com.example.ta4;
 
 import android.content.Context;
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-@Database(entities = {Gasto.class, Categoria.class, Usuario.class}, version = 3)
+@Database(entities = {Gasto.class, Categoria.class, Usuario.class}, version = 4)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract GastoDao gastoDao();
@@ -14,6 +18,9 @@ public abstract class AppDatabase extends RoomDatabase {
 
     private static volatile AppDatabase INSTANCE;
 
+    private static final ExecutorService databaseWriteExecutor =
+            Executors.newSingleThreadExecutor();
+
     public static AppDatabase getInstance(final Context context) {
         if (INSTANCE == null) {
             synchronized (AppDatabase.class) {
@@ -21,10 +28,30 @@ public abstract class AppDatabase extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                                     AppDatabase.class, "gastos_database")
                             .fallbackToDestructiveMigration()
+                            .addCallback(roomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
     }
+
+    private static RoomDatabase.Callback roomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            databaseWriteExecutor.execute(() -> {
+                CategoriaDao dao = INSTANCE.categoriaDao();
+                dao.insert(new Categoria("Comida"));
+                dao.insert(new Categoria("Transporte"));
+                dao.insert(new Categoria("Salud"));
+                dao.insert(new Categoria("Compras"));
+                dao.insert(new Categoria("Ocio"));
+                dao.insert(new Categoria("Educación"));
+                dao.insert(new Categoria("Hogar"));
+                dao.insert(new Categoria("Otros"));
+            });
+        }
+    };
 }
